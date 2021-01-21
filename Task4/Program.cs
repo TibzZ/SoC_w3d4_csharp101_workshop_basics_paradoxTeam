3e2c8a93e0924b2057bdafd679485481a27cdb76
ParadigmZero <josephrcalton@gmail.com>
2021-01-21 14:15:51 +0000

    Task 4 - done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -12,11 +12,12 @@ class Program
             // based on divisibility
             if(i % 3 == 0 && i % 5 ==0) 
             {
-                Console.WriteLine("FizzBuzz");
-            }
-            else if(i % 3 ==0)
-            {
-                Console.WriteLine("Fizz");
+                Console.Write("Fizz");
+
+                if(i % 5 ==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
             }
             else if(i % 5 ==0)
             {
