1985036831acef883f3839c4c4792bad65208919
Tibz <thibaut_marechal@yahoo.com>
2021-04-21 21:19:44 +0100

    Task 1 C# practise

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,10 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        // add input to string
-        string username = Console.ReadLine();
-        // print out message
-        Console.WriteLine($"Hello {username}!");
+        // // add input to string
+        // string username = Console.ReadLine();
+        // // print out message
+        // Console.WriteLine($"Hello {username}!");
+
+        Console.Write("Please enter your name:"); //clarify for user to enter a name and keep next command ReadLine on one visual line
+        string nameUser = Console.ReadLine();
+        Console.WriteLine($"Hello {nameUser} :)");
+
+     
+  
+
     }
 }
 
