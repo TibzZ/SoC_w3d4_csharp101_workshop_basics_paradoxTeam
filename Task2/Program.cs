6515dbf1f4849e8354d32cde1f7f9104389d9246
Tibz <thibaut_marechal@yahoo.com>
2021-04-21 22:48:14 +0100

    Task 2 complete, function determine of user name lenght is odd or even and trigger tailored message

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Task 2!");
+        // Console.WriteLine("Hello Task 2!");
+
+        Console.Write("I will need to ask your name again, sorry, what is it please?: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine($"Your name is {userName}, that sounds so cool!");
+
+        if(userName.Length % 2 == 0 ){
+            Console.WriteLine($"Hello {userName}, you'll soon be a dotnet EVENgelist");
+
+        } else {
+        Console.WriteLine($"Hello {userName} you'll soon be an dODDnet Evangelist");
+        }
+
     }
 }
