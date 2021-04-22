90f97417e302aa7e2964cfc6d7849386a400234d
Tibz <thibaut_marechal@yahoo.com>
2021-04-22 09:02:07 +0100

    Task3 complete, practise while loop

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Task 3!");
+        int myCounter = 0;
+
+        while(myCounter < 15){
+            Console.WriteLine(myCounter);
+            myCounter++;
+        }
+
+
+
     }
 }
