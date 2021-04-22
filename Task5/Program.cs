8db1052a69e01a8370bf028c7c8e68a004eb809d
Tibz <thibaut_marechal@yahoo.com>
2021-04-22 10:06:59 +0100

    Task 5, part 2 complete, to investigat MS doc on random further

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,6 +5,10 @@ class Program
     static void Main(string[] args)
     {
         var rand = new Random();
+
+        // Generate and display 5 random byte (integer) values.
+       Console.WriteLine($"{rand.Next(0,6)}");
+
     }
 }
 
