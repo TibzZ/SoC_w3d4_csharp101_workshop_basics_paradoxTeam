11c4e850c9edd8cc8cd078158f9d76d2e85d926d
Tibz <thibaut_marechal@yahoo.com>
2021-04-22 09:31:52 +0100

    Task 4 FizzBuzz complete

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,30 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        // print 0-100
-        for(int i=0; i < 101; i++)
-        {
-            // make some conditions/
-            // to print Fizz and/or Buzz
-            // based on divisibility
-            if(i % 3 == 0 && i % 5 ==0) 
-            {
-                Console.Write("Fizz");
+        
+        // //Step 1 - Print numbers from 0 to 100 using a loop
+        // for(int i = 0; i <= 100 ; i++){
+        //     Console.WriteLine($".... and....{i}");
+        // }
 
-                if(i % 5 ==0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-            }
-            else if(i % 5 ==0)
-            {
-                Console.WriteLine("Buzz");
-            }
-            else
-            {
+        //Step 2 - Recreate previous loop and iterate through it: 
+        for (int i = 0; i <= 100; i++){
+           
+            if(i == 0){
                 Console.WriteLine(i);
-            }
-        }
+            } else if(i % 3 == 0 && i % 5 == 0){
+                Console.WriteLine("FizzBuzz");
+            } else if(i % 3 == 0){
+                Console.WriteLine("Fizz");
+            } else if (i % 5 == 0){
+                Console.WriteLine("Buzz");
+            } else {
+                Console.WriteLine(i);
+            };
+
+        }        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // print 0-100
+        // for(int i=0; i < 101; i++)
+        // {
+        //     // make some conditions/
+        //     // to print Fizz and/or Buzz
+        //     // based on divisibility
+        //     if(i % 3 == 0 && i % 5 ==0) 
+        //     {
+        //         Console.Write("Fizz");
+
+        //         if(i % 5 ==0)
+        //         {
+        //             Console.WriteLine("Buzz");
+        //         }
+        //     }
+        //     else if(i % 5 ==0)
+        //     {
+        //         Console.WriteLine("Buzz");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine(i);
+        //     }
+        // }
 
 
 
